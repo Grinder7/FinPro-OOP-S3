da3cb70e678907914c8029b84439f6a96aa4b839
bryanfks-dev <bryanfernandoks11@gmail.com>
2023-12-13 09:16:50 +0700

    commit

---
+++ src/app/controllers/DashboardController.java
@@ -0,0 +1,18 @@
+package app.controllers;
+
+// Javafx lib
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DashboardController {
+
+    @FXML
+    private Label avail_caretaker;
+
+    @FXML
+    private Label filled_cap_label;
+
+    @FXML
+    private Label patient_total;
+
+}
